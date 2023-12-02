2d1cea0b4d9faa99792f3a45c181530887f8c90e
boradoriii <syjoo0126@naver.com>
2023-12-03 00:08:36 +0900

    1-2

--- main.c
+++ main.c
@@ -12,5 +12,16 @@ struct student{
 int main(int argc, char *argv[]) {
 	struct student s1 = {123, "SeoYun", 4.3};
 	
+	s1.ID = 123456;
+	s1.name[0] = 'c';
+	s1.score = 0.7;
+	
+	printf("ID : %d\n", s1.ID);
+	printf("name : %s\n", s1.name);
+	printf("score : %f\n", s1.score);
+	
+	strcpy(s1.name, "K");
+	printf("name2 : %s\n", s1.name);
+	
 	return 0;
 }
